b955ce0a2a25fc02f4cebd08fe494d45b1ff4cc4
Andréas CASTELLO <andreas.castello06@gmail.com>
2023-05-23 10:02:30 +0200

    last-commit

--- App.js
+++ App.js
@@ -2,13 +2,37 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import TabNavigator from "./Navigation/TabNavigator";
+import * as Notifications from 'expo-notifications';
+import * as Permissions from "expo-permissions";
+import {useEffect} from "react";
 
+// async function registerForPushNotificationsAsync() {
+//     const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+//     if (status !== 'granted') {
+//         alert('No notification permissions!');
+//         return;
+//     }
+//     let token = await Notifications.getExpoPushTokenAsync();
+//     console.log(token);
+// }
+
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: false,
+        shouldSetBadge: false,
+    }),
+});
 
 function App() {
   return (
+      // useEffect(() => {
+      //       registerForPushNotificationsAsync().then(token => console.log(token)).catch(err => console.log(err));
+      //   }, []),
+
       <NavigationContainer>
         <TabNavigator />
-        </NavigationContainer>
+      </NavigationContainer>
   );
 }
 
--- Navigation/Pages/Profile/Profile.jsx
+++ Navigation/Pages/Profile/Profile.jsx
@@ -1,12 +1,27 @@
 import React from "react";
-import { Text, View, ScrollView, StyleSheet } from "react-native";
+import {Text, View, ScrollView, StyleSheet, TouchableOpacity} from "react-native";
 import externalStyle from "../../../style/externalStyle";
-
+import * as Notifications from "expo-notifications";
+import * as Permissions from "expo-permissions";
 
 const Profile = () => {
+    const triggerNotificationHandler = () => {
+        Notifications.scheduleNotificationAsync({
+            content: {
+                title: "Notification de test",
+                body: "Une notif oh oui !",
+                data: { mySpecialData: 'Some text' },
+            },
+            trigger: null,
+        });
+    };
+
     return (
         <View style={[externalStyle.bg]}>
             <Text>Profile</Text>
+            <TouchableOpacity title="Send Notification" onPress={triggerNotificationHandler}>
+                <Text>Send Notification</Text>
+            </TouchableOpacity>
         </View>
     );
 };
